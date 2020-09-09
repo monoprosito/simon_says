34452810af3845471cfa132cc440dd32192fd01f
Santiago <monoprosito@gmail.com>
2020-09-09 18:17:04 -0500

    Generates a random sequence to show the colors

--- index.js
+++ index.js
@@ -7,13 +7,25 @@ const btnEmpezar = document.getElementById('btnEmpezar')
 class Juego {
     constructor() {
         this.inicializar()
+        this.generarSecuencia()
     }
 
     inicializar() {
+        this.nivel = 1
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
         btnEmpezar.classList.add('hide')
+    }
+
+    generarSecuencia() {
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
     }
 }
 
 function empezarJuego() {
-    let juego = new Juego()
+    window.juego = new Juego()
 }
